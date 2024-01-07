3091c6bd122e7de4cf4bd832fdb05bf251939d4e
liuzhao <1257625996@qq.com>
2024-01-08 00:18:02 +0800

    拆分登录组件

--- webview-ui/src/App.vue
+++ webview-ui/src/App.vue
@@ -1,38 +1,27 @@
 <script setup lang="ts">
-import { provideVSCodeDesignSystem, vsCodeButton } from "@vscode/webview-ui-toolkit";
-import { vscode } from "./utilities/vscode";
+  import { provideVSCodeDesignSystem, vsCodeButton, vsCodeTextField } from "@vscode/webview-ui-toolkit";
+  import { vscode } from "./utilities/vscode";
+  import { ref } from "vue";
+  import Login from "@/components/Login.vue";
 
-// In order to use the Webview UI Toolkit web components they
-// must be registered with the browser (i.e. webview) using the
-// syntax below.
-provideVSCodeDesignSystem().register(vsCodeButton());
+  provideVSCodeDesignSystem().register(
+    vsCodeButton(),
+    vsCodeTextField()
+  );
 
-// To register more toolkit components, simply import the component
-// registration function and call it from within the register
-// function, like so:
-//
-// provideVSCodeDesignSystem().register(
-//   vsCodeButton(),
-//   vsCodeCheckbox()
-// );
-//
-// Finally, if you would like to register all of the toolkit
-// components at once, there's a handy convenience function:
-//
-// provideVSCodeDesignSystem().register(allComponents);
-
-function handleHowdyClick() {
-  vscode.postMessage({
-    command: "hello",
-    text: "Hey there partner! 🤠",
-  });
-}
+  function handleHowdyClick() {
+    vscode.postMessage({
+      command: "hello",
+      text: "Hey there partner! 🤠",
+    });
+  }
+  const hasSession = ref(false);
 </script>
 
 <template>
   <main>
-    <h1>Hello world!</h1>
-    <vscode-button @click="handleHowdyClick">Howdy!</vscode-button>
+    <div v-if="hasSession">main</div>
+    <Login v-else />
   </main>
 </template>
 
---
+++ webview-ui/src/components/Login.vue
@@ -0,0 +1,27 @@
+<script setup lang="ts">
+  import { reactive } from "vue";
+
+  const formData = reactive({
+    url: 'https://192.168.112.12',
+    username: 'admin',
+    password: 'Huawei@123',
+  })
+  const handleLogin = () => {
+    console.log(formData);
+  }
+</script>
+
+<template>
+  <div>
+    <form @submit="handleLogin">
+      <vscode-text-field type="url" v-model="formData.url">网址</vscode-text-field>
+      <vscode-text-field type="text" v-model="formData.username">用户名</vscode-text-field>
+      <vscode-text-field type="text" v-model="formData.password">密码</vscode-text-field>
+      <vscode-button type="submit">登录 <span class="codicon codicon-check"></span></vscode-button>
+    </form>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
